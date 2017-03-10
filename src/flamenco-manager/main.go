698ef7c2c3a3d029fc49ba5161015f0152dbfd42
Sybren A. Stüvel <sybren@stuvel.eu>
2017-03-10 12:16:43 +0100

    Bumped version to 2.0-beta10-manager3

--- src/flamenco-manager/main.go
+++ src/flamenco-manager/main.go
@@ -22,7 +22,7 @@ import (
 	"github.com/gorilla/mux"
 )
 
-const FLAMENCO_VERSION = "2.0-beta10-manager2"
+const FLAMENCO_VERSION = "2.0-beta10-manager3"
 
 // MongoDB session
 var session *mgo.Session
