8afc05773d194ca81ee3b7b90c25ebcaba8e9b66
Sybren A. Stüvel <sybren@stuvel.eu>
2017-03-23 13:08:20 +0100

    Work in progress on supporting cross-compilation

--- src/flamenco-manager/docker/build-via-docker.sh
+++ src/flamenco-manager/docker/build-via-docker.sh
@@ -10,8 +10,24 @@ docker run -i --rm \
     --env GOPATH=/go-local \
      golang /bin/bash -e << EOT
 go version
+set -x
 cd \${GOPATH}/src/flamenco-manager
-CGO_ENABLED=0 go get -a -ldflags '-s'
-cp \${GOPATH}/bin/flamenco-manager /docker
-chown $UID:$GID /docker/flamenco-manager
+
+function build {
+    export GOOS=\$1
+    export GOARCH=\$2
+    export SUFFIX=\$3
+    TARGET=flamenco-manager-\$GOOS-\$GOARCH\$SUFFIX
+
+    echo "Building \$TARGET"
+    go get -a -ldflags '-s'
+    go build -o /docker/\$TARGET
+
+    chown $UID:$GID /docker/\$TARGET
+}
+
+export CGO_ENABLED=0
+build linux amd64
+build windows amd64 .exe
+build darwin amd64
 EOT
