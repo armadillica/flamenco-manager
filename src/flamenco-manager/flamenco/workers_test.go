7e82e9bf78952b0fc289123533ed1090f2465ca5
Sybren A. Stüvel <sybren@stuvel.eu>
2017-03-31 16:46:59 +0200

    Made workers_test.go its own test suite
    Previously it hooked into the scheduler test suite, which was a bit
    confusing.

--- src/flamenco-manager/flamenco/workers_test.go
+++ src/flamenco-manager/flamenco/workers_test.go
@@ -6,11 +6,62 @@ import (
 	"net/http"
 	"net/http/httptest"
 
+	log "github.com/Sirupsen/logrus"
 	auth "github.com/abbot/go-http-auth"
 	"github.com/stretchr/testify/assert"
 	check "gopkg.in/check.v1"
+	httpmock "gopkg.in/jarcoal/httpmock.v1"
+	mgo "gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
+
+type WorkerTestSuite struct {
+	workerLnx Worker
+	workerWin Worker
+
+	db       *mgo.Database
+	upstream *UpstreamConnection
+	sched    *TaskScheduler
+}
+
+var _ = check.Suite(&WorkerTestSuite{})
+
+func (s *WorkerTestSuite) SetUpTest(c *check.C) {
+	httpmock.Activate()
+
+	config := GetTestConfig()
+	session := MongoSession(&config)
+	s.db = session.DB("")
+
+	s.upstream = ConnectUpstream(&config, session)
+	s.sched = CreateTaskScheduler(&config, s.upstream, session)
+
+	// Store workers in DB, on purpose in the opposite order as the tasks.
+	s.workerLnx = Worker{
+		Platform:           "linux",
+		SupportedTaskTypes: []string{"sleeping"},
+		Nickname:           "workerLnx",
+	}
+	if err := StoreNewWorker(&s.workerLnx, s.db); err != nil {
+		c.Fatal("Unable to insert test workerLnx", err)
+	}
+	s.workerWin = Worker{
+		Platform:           "windows",
+		SupportedTaskTypes: []string{"testing"},
+		Nickname:           "workerWin",
+	}
+	if err := StoreNewWorker(&s.workerWin, s.db); err != nil {
+		c.Fatal("Unable to insert test workerWin", err)
+	}
+
+}
+
+func (s *WorkerTestSuite) TearDownTest(c *check.C) {
+	log.Info("WorkerTestSuite tearing down test, dropping database.")
+	s.upstream.Close()
+	s.db.DropDatabase()
+	httpmock.DeactivateAndReset()
+}
 
 func WorkerTestRequest(workerID bson.ObjectId, method, url string, vargs ...interface{}) (*httptest.ResponseRecorder, *auth.AuthenticatedRequest) {
 	return WorkerTestRequestWithBody(workerID, nil, method, url, vargs...)
@@ -38,7 +89,7 @@ func WorkerTestRequestWithBody(workerID bson.ObjectId, body io.Reader, method, u
 	return respRec, ar
 }
 
-func (s *SchedulerTestSuite) TestWorkerMayRun(t *check.C) {
+func (s *WorkerTestSuite) TestWorkerMayRun(t *check.C) {
 	// Store task in DB.
 	task := ConstructTestTask("aaaaaaaaaaaaaaaaaaaaaaaa", "sleeping")
 	if err := s.db.C("flamenco_tasks").Insert(task); err != nil {
