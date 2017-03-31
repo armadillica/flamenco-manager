5c16385f694f6eade55b82d9a3b06b1d14e9f35b
Sybren A. Stüvel <sybren@stuvel.eu>
2017-03-31 15:40:05 +0200

    Fixed some linter issues

--- src/flamenco-manager/flamenco/gocheck_test.go
+++ src/flamenco-manager/flamenco/gocheck_test.go
@@ -14,13 +14,13 @@ import (
 // You only need one of these per package, or tests will run multiple times.
 func TestWithGocheck(t *testing.T) { check.TestingT(t) }
 
-func ConstructTestTask(task_id, job_type string) Task {
-	return ConstructTestTaskWithPrio(task_id, job_type, 50)
+func ConstructTestTask(taskID, JobType string) Task {
+	return ConstructTestTaskWithPrio(taskID, JobType, 50)
 }
 
-func ConstructTestTaskWithPrio(task_id, job_type string, priority int) Task {
+func ConstructTestTaskWithPrio(taskID, JobType string, priority int) Task {
 	return Task{
-		ID:       bson.ObjectIdHex(task_id),
+		ID:       bson.ObjectIdHex(taskID),
 		Etag:     "1234567",
 		Job:      bson.ObjectIdHex("bbbbbbbbbbbbbbbbbbbbbbbb"),
 		Manager:  bson.ObjectIdHex("cccccccccccccccccccccccc"),
@@ -29,7 +29,7 @@ func ConstructTestTaskWithPrio(task_id, job_type string, priority int) Task {
 		Name:     "Test task",
 		Status:   "queued",
 		Priority: priority,
-		JobType:  job_type,
+		JobType:  JobType,
 		Commands: []Command{
 			Command{"echo", bson.M{"message": "Running Blender from {blender}"}},
 			Command{"sleep", bson.M{"time_in_seconds": 3}},
