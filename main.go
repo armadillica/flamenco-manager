fc227fb3bf7a669d5347512b80dd89a1dc0b5aaf
Sybren A. Stüvel <sybren@stuvel.eu>
2018-12-14 11:33:44 +0100

    Bumped version to 2.3-dev1

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ import (
 	"github.com/gorilla/mux"
 )
 
-const flamencoVersion = "2.3-dev0"
+const flamencoVersion = "2.3-dev1"
 const ssdpServiceType = "urn:flamenco:manager:0"
 
 // MongoDB session
