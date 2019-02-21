1143a5b957d37a7d82b613d1cc0fbd8961c44685
Sybren A. Stüvel <sybren@stuvel.eu>
2019-02-21 13:45:39 +0100

    Bumped version to 2.4-dev4

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ import (
 	"gitlab.com/blender-institute/gossdp"
 )
 
-const flamencoVersion = "2.4-dev3"
+const flamencoVersion = "2.4-dev4"
 const ssdpServiceType = "urn:flamenco:manager:0"
 
 var (
