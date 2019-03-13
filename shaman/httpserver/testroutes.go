a34e0c1b5f1f20fde52c5e0f4eca4db00f8b0425
Sybren A. Stüvel <sybren@stuvel.eu>
2019-03-13 14:41:25 +0100

    Removed some test endpoints

--- shaman/httpserver/testroutes.go
+++ shaman/httpserver/testroutes.go
@@ -25,77 +25,12 @@ package httpserver
  */
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/armadillica/flamenco-manager/shaman/auth"
 	"github.com/gorilla/mux"
 )
-
-// Product is our test API payload
-type Product struct {
-	ID          int
-	Name        string
-	Slug        string
-	Description string
-}
-
-var products = []Product{
-	Product{ID: 1, Name: "Hover Shooters", Slug: "hover-shooters", Description: "Shoot your way to the top on 14 different hoverboards"},
-	Product{ID: 2, Name: "Ocean Explorer", Slug: "ocean-explorer", Description: "Explore the depths of the sea in this one of a kind underwater experience"},
-	Product{ID: 3, Name: "Dinosaur Park", Slug: "dinosaur-park", Description: "Go back 65 million years in the past and ride a T-Rex"},
-	Product{ID: 4, Name: "Cars VR", Slug: "cars-vr", Description: "Get behind the wheel of the fastest cars in the world."},
-	Product{ID: 5, Name: "Robin Hood", Slug: "robin-hood", Description: "Pick up the bow and arrow and master the art of archery"},
-	Product{ID: 6, Name: "Real World VR", Slug: "real-world-vr", Description: "Explore the seven wonders of the world in VR"},
-}
-
-// Here we are implementing the notImplemented handler. Whenever an API endpoint is hit
-// we will simply return the message "Not Implemented"
-var notImplemented = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNotImplemented)
-	w.Write([]byte("Not Implemented"))
-})
-
-/* The status handler will be invoked when the user calls the /status route
-It will simply return a string with the message "API is up and running" */
-var statusHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("API is up and running"))
-})
-
-/* The products handler will be called when the user makes a GET request to the /products endpoint.
-This handler will return a list of products available for users to review */
-var productsHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	// Here we are converting the slice of products to JSON
-	payload, _ := json.Marshal(products)
-
-	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(payload))
-})
-
-/* The feedback handler will add either positive or negative feedback to the product
-We would normally save this data to the database - but for this demo, we'll fake it
-so that as long as the request is successful and we can match a product to our catalog of products
-we'll return an OK status. */
-var addFeedbackHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	var product Product
-	vars := mux.Vars(r)
-	slug := vars["slug"]
-
-	for _, p := range products {
-		if p.Slug == slug {
-			product = p
-		}
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	if product.Slug != "" {
-		payload, _ := json.Marshal(product)
-		w.Write([]byte(payload))
-	} else {
-		w.Write([]byte("Product Not Found"))
-	}
-})
 
 var userInfo = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 	tokenSubject, ok := auth.SubjectFromContext(r.Context())
@@ -115,15 +50,6 @@ func RegisterTestRoutes(r *mux.Router, auther auth.Authenticator) {
 	// We will setup our server so we can serve static assest like images, css from the /static/{file} route
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 
-	// Our API is going to consist of three routes
-	// /status - which we will call to make sure that our API is up and running
-	// /products - which will retrieve a list of products that the user can leave feedback on
-	// /products/{slug}/feedback - which will capture user feedback on products
-	r.Handle("/status", statusHandler).Methods("GET")
-	r.Handle("/my-info", auther.Wrap(userInfo)).Methods("GET")
-	r.Handle("/products", auther.Wrap(productsHandler)).Methods("GET")
-	r.Handle("/products/{slug}/feedback", auther.Wrap(addFeedbackHandler)).Methods("POST")
-
 	getTokenHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		tokenString, err := auther.GenerateToken()
 		if err != nil {
@@ -137,4 +63,5 @@ func RegisterTestRoutes(r *mux.Router, auther auth.Authenticator) {
 	})
 
 	r.Handle("/get-token", getTokenHandler).Methods("GET")
+	r.Handle("/my-info", auther.Wrap(userInfo)).Methods("GET")
 }
