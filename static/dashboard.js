99d8693c1144bc1a779e46fae89d6a42fea83a5b
Sybren A. Stüvel <sybren@stuvel.eu>
2019-03-18 11:30:07 +0100

    Added MIT license text to JavaScript files

--- static/dashboard.js
+++ static/dashboard.js
@@ -1,3 +1,29 @@
+/* ***** BEGIN MIT LICENSE BLOCK *****
+ * (c) 2019, Blender Foundation - Sybren A. Stüvel
+ *
+ * This file is part of Flamenco Manager.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * ***** END MIT LICENCE BLOCK *****
+ */
+
 var clipboard;
 
 // So we can call window.clearTimeout(load_workers_timeout_handle)
@@ -437,6 +463,10 @@ var vueApp = new Vue({
                     load_workers_timeout_handle = setTimeout(this.loadWorkers, 2000);
                 })
                 .fail(error => {
+                    if (error.status == 401) {
+                        getJWTToken().then(this.loadWorkers);
+                        return;
+                    }
                     if (error.status) {
                         this.errormsg = 'Error ' + error.status + ': ' + error.responseText;
                     } else {
